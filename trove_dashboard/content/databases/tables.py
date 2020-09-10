f478aa90c1d0eac5c71e9def519aabc0d095b4cd
Ivan Kolodyazhny <e0ne@e0ne.info>
2020-09-10 13:21:01 +0300

    Show 'HEALTHY' status in the database service list as a final
    Starting with Ussuri 'HEALTHY' is the final status after 'ACTIVE' so we
    don't need to show a progress bar in the dashboard.
    
    Change-Id: Iccb1bc78939545a9b8c605efe01c6167c8553597

--- trove_dashboard/content/databases/tables.py
+++ trove_dashboard/content/databases/tables.py
@@ -34,7 +34,7 @@ from trove_dashboard.content.database_backups \
     import tables as backup_tables
 
 
-ACTIVE_STATES = ("ACTIVE",)
+ACTIVE_STATES = ("ACTIVE", "HEALTHY")
 
 
 class DeleteInstance(tables.DeleteAction):
@@ -627,6 +627,7 @@ def get_databases(user):
 class InstancesTable(tables.DataTable):
     STATUS_CHOICES = (
         ("ACTIVE", True),
+        ("HEALTHY", True),
         ("BLOCKED", True),
         ("BUILD", None),
         ("FAILED", False),
@@ -641,6 +642,8 @@ class InstancesTable(tables.DataTable):
     STATUS_DISPLAY_CHOICES = (
         ("ACTIVE", pgettext_lazy("Current status of a Database Instance",
                                  u"Active")),
+        ("Healthy", pgettext_lazy("Current status of a Database Instance",
+                                  u"Healthy")),
         ("BLOCKED", pgettext_lazy("Current status of a Database Instance",
                                   u"Blocked")),
         ("BUILD", pgettext_lazy("Current status of a Database Instance",
